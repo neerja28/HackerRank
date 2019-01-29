1827bd43ed424518291c11c63c7f1c2c90610e6b
Neerja Narayanappa <n.neerja28@gmail.com>
2019-01-29 08:28:19 -0600

    Create runner_up_score.py

---
+++ Basic_Data_Types/runner_up_score.py
@@ -0,0 +1,23 @@
+# Find the Second Largest Number
+
+# You are given N numbers. Store them in a list and find the second largest number.
+
+# Input Format 
+# The first line contains N. The second line contains an array A[] of N integers each separated by a space.
+
+# Constraints
+# 2 <= N <= 10
+# -100 <= A[i] <= 100
+
+# Output Format 
+# Output the value of the second largest number.
+
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+if __name__ == '__main__':
+    n = int(input())
+    map_arr = map(int, input().split())
+    set_arr = set(map_arr)
+    list_arr = sorted(list(set_arr))
+    print (list_arr[-2])
