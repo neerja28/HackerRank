a5a3d9cf7b44b1aba87128f813ac5c24a134f650
Neerja Narayanappa <n.neerja28@gmail.com>
2019-02-06 08:07:01 -0600

    Create Set_Mutations.py

---
+++ sets/Set_Mutations.py
@@ -0,0 +1,68 @@
+We have seen the applications of union, intersection, difference and symmetric difference operations, but these operations do not make any changes or mutations to the set.
+
+We can use the following operations to create mutations to a set:
+
+.update() or |= 
+Update the set by adding elements from an iterable/another set.
+
+>>> H = set("Hacker")
+>>> R = set("Rank")
+>>> H.update(R)
+>>> print H
+set(['a', 'c', 'e', 'H', 'k', 'n', 'r', 'R'])
+.intersection_update() or &=
+Update the set by keeping only the elements found in it and an iterable/another set.
+
+>>> H = set("Hacker")
+>>> R = set("Rank")
+>>> H.intersection_update(R)
+>>> print H
+set(['a', 'k'])
+.difference_update() or -=
+Update the set by removing elements found in an iterable/another set.
+
+>>> H = set("Hacker")
+>>> R = set("Rank")
+>>> H.difference_update(R)
+>>> print H
+set(['c', 'e', 'H', 'r'])
+.symmetric_difference_update() or ^=
+Update the set by only keeping the elements found in either set, but not in both.
+
+>>> H = set("Hacker")
+>>> R = set("Rank")
+>>> H.symmetric_difference_update(R)
+>>> print H
+set(['c', 'e', 'H', 'n', 'r', 'R'])
+TASK
+You are given a set A and  N number of other sets. These  number of sets have to perform some specific mutation operations on set .
+
+Your task is to execute those operations and print the sum of elements from set A.
+
+Input Format
+
+The first line contains the number of elements in set A.
+The second line contains the space separated list of elements in set A.
+The third line contains integer , the number of other sets.
+The next  lines are divided into  parts containing two lines each.
+The first line of each part contains the space separated entries of the operation name and the length of the other set.
+The second line of each part contains space separated list of elements in the other set.
+
+ len(set(A))  
+ len(otherSets)  
+
+Output Format
+
+Output the sum of elements in set A.
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+(_, A) = (int(input()), set(map(int,input().split())))
+num_of_commands = int(input())
+
+for _ in range(num_of_commands):
+    command, B = input().split()[0], set(map(int,input().split()))
+    getattr(A, command)(B)
+    # The only thing I understand about getattr() is that getattr(li, "pop") or (A,command) is the same as calling li.pop or A.intersection(B)
+
+print (sum(A))
