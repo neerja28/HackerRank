918441629591e63d741f5f354ab08424e80d9a47
Neerja Narayanappa <n.neerja28@gmail.com>
2020-01-30 16:13:16 -0600

    Update functions.py

--- Introduction/functions.py
+++ Introduction/functions.py
@@ -37,3 +37,24 @@ def is_leap(year):
 
 year = int(input())
 print(is_leap(year))
+
+
+
+# Better Code
+
+def is_leap(year):
+    leap = False
+    if (year%4 == 0):
+        if (year%100 == 0):
+            if(year%400 == 0):
+                leap=True
+            else:
+                leap=False
+        else:
+            leap=True
+    else:
+        leap=False
+    # Write your logic here
+    return leap
+
+year = int(input())
